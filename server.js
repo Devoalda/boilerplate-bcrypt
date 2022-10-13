db943e3e9d8da4236b8724dc63929976e2964b25
Glitch (discreet-incredible-sardine) <none>
2022-10-13 03:56:26 +0000

    🧦💭 Updated with Glitch

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcrypt');
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
@@ -11,13 +12,21 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
 
 //END_ASYNC
 
 //START_SYNC
 
-
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
